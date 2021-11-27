efcac19d19ea771cee0fa8b118b5c76bcb7494f6
moontia04 <moontia04@gmail.com>
2021-11-28 07:40:18 +0900

    데이터 받기위한 커밋

--- GameJamTrail/Assets/Scripts/HH/CField.cs
+++ GameJamTrail/Assets/Scripts/HH/CField.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class CField : MonoBehaviour, IMove
 {
     private IFieldControl fieldControl;
     [SerializeField] private TMP_Text mText;
-    [SerializeField] private GameObject riverOb;
     [SerializeField] private GameObject finishOb;
     [SerializeField] private GameObject signOb;
     [SerializeField] private GameObject textOb;
+
+    [SerializeField] private GameObject[] presets;
+
+    private GameObject preset;
 
     public void SetField(IFieldControl fieldControl, int m, bool riverOn, bool finish)
     {
@@ -19,8 +20,12 @@ public class CField : MonoBehaviour, IMove
         textOb.SetActive(m % 100 == 0);
         mText.text = m +"M";
         CGameManager.Instance.AddMove(this);
-        riverOb.SetActive(riverOn);
         finishOb.SetActive(finish);
+
+        preset = CObjectPool.instance.GetObject(presets[Random.Range(0, presets.Length)]);
+        preset.transform.SetParent(transform);
+        preset.transform.localPosition = Vector3.zero;
+
     }
 
     public void SetField(IFieldControl fieldControl, int m, float startZPos, bool riverOn, bool finish)
@@ -37,6 +42,11 @@ public class CField : MonoBehaviour, IMove
         {
             CGameManager.Instance.RemoveMove(this);
             fieldControl.FieldMoveEnd(this);
+            if (preset != null)
+            {
+                CObjectPool.instance.PoolObject(preset);
+                preset = null;
+            }
         }
 
     }
