b1b2d28a8c45aa5c67eac69caeeb10f59049fc08
ChoiHanMin <iskierka1013@naver.com>
2021-11-28 07:16:22 +0900

    작업 커밋

--- GameJamTrail/Assets/Scripts/HM/RankingScript.cs
+++ GameJamTrail/Assets/Scripts/HM/RankingScript.cs
@@ -1,24 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
 using com.ootii.Messages;
 
 public class RankingScript : MonoBehaviour
 {
-    public Text[] UserNames;
-    public Text[] ClearTimes;
 
     // Start is called before the first frame update
     void Start()
     {
-        MessageDispatcher.AddListener("SetUserInfos", SetUserInfos);
+       
     }
 
     private void OnDestroy()
     {
-        MessageDispatcher.RemoveListener("SetUserInfos", SetUserInfos);
+       
     }
     // Update is called once per frame
     void Update()
@@ -26,37 +23,8 @@ public class RankingScript : MonoBehaviour
 
     }
 
-    private void SetUserInfos(IMessage rMessage)
+    private void SetUserInfos()
     {
-        this.GetComponent<DOTweenAnimation>().DOPlay();
-        if (UserNames != null)
-        {
-            for (int i = 0; i < UserNames.Length; i++)
-            {
-                if (JsonManager.Instance.UserList.Count - 1 >= i)
-                {
-                    UserNames[i].text = JsonManager.Instance.UserList[i].UserName;
-                }
-                else
-                {
-                    UserNames[i].text = "";
-                }
-            }
-        }
-
-        if (ClearTimes != null)
-        {
-            for (int i = 0; i < ClearTimes.Length; i++)
-            {
-                if (JsonManager.Instance.UserList.Count - 1 >= i)
-                {
-                    ClearTimes[i].text = JsonManager.Instance.UserList[i].ClearTime.ToString();
-                }
-                else
-                {
-                    ClearTimes[i].text = "";
-                }
-            }
-        }
+       
     }
 }
---
+++ GameJamTrail/Assets/Scripts/HM/SparkColi.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using com.ootii.Messages;
+
+public class SparkColi : MonoBehaviour, IPointerEnterHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("bbbbbbbbbbbb");
+        MessageDispatcher.SendMessage("ImageIn");
+    }
+
+}
