044edb4c2940e7c622148a2932f215bac597ac9c
moontia04 <moontia04@gmail.com>
2021-11-27 14:46:56 +0900

    FIrst2

--- GameJamTrail/Assets/Scripts/HH/CGameManager.cs
+++ GameJamTrail/Assets/Scripts/HH/CGameManager.cs
@@ -5,15 +5,14 @@ using UnityEngine.UI;
 using DG.Tweening;
 
 
-public class CGameManager_HM : MonoBehaviour
+public class CGameManager : MonoBehaviour
 {
-    private static CGameManager_HM instance;
-    public static CGameManager_HM Instance { get { return instance; } }
+    private static CGameManager instance;
+    public static CGameManager Instance { get { return instance; } }
 
     private List<IMove> moveList = new List<IMove>();
 
     private bool isMove = false;
-<<<<<<< HEAD
     // 1ÃÊ¿¡ 1.7m 
     private float speed = 1f;
     [Range(1f, 120f)]
@@ -22,16 +21,7 @@ public class CGameManager_HM : MonoBehaviour
     private float[] kmsLevel = { 1f, 5f, 10f, 15f };
     private int level = 0;
         
-=======
     private float spped = 1f;
-
-
-    // MH_ADD
-    private float GameTimer;
-    public Text TimeText;
-    // public Text[] ScrambleTexts;
-    //Coroutine Timer;
->>>>>>> e19b129c32c51deefbccf81d4693a46aee7a75a0
 
     public void AddMove(IMove move)
     {
@@ -62,14 +52,35 @@ public class CGameManager_HM : MonoBehaviour
         {
             for (int i = 0; i < moveList.Count; i++)
             {
-                moveList[i].Move(0);
+                moveList[i].Move(Time.deltaTime * speed * kms);
             }
         }
-        if (GameTimer < 30)
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameTimer += Time.deltaTime;
-            TimeText.text = string.Format("{0:N2}", GameTimer);
+            isMove = !isMove;
         }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (level < kmsLevel.Length - 1)
+            {
+                level++;
+                kms = kmsLevel[level];
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (level > 0)
+            {
+                level--;
+                kms = kmsLevel[level];
+            }
+        }
+
+
     }
 
 
